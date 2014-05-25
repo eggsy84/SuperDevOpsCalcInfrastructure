678b5887b9f328fd002f78767807bbd735d9b08c
eggsy84 <eggsy@eggsylife.co.uk>
2014-05-25 12:59:46 +0100

    Ignore Vagrant temp directory and refactor puppet file to use sinlge quotes as interpolation not necessary

--- puppet/modules/nginx/manifests/init.pp
+++ puppet/modules/nginx/manifests/init.pp
@@ -1,23 +1,23 @@
 class nginx {
   
-  exec { "apt-get update":
-    command => "/usr/bin/apt-get update",
+  exec { 'apt-get update':
+    command => '/usr/bin/apt-get update',
   }
   
   package { 'apache2.2-common':
-    require => Exec["apt-get update"],
+    require => Exec['apt-get update'],
     ensure => absent,
   }
   
   package { 'nginx' :
     ensure => installed,
-    require => Package["apache2.2-common"],
+    require => Package['apache2.2-common'],
   }
 	
   service { 'nginx':
     ensure => running,
     enable => true,
-    require => Package["nginx"],
+    require => Package['nginx'],
   }
   
   file { '/etc/nginx/sites-enabled/puppetagent.dev':
@@ -26,8 +26,8 @@ class nginx {
     require => Package['nginx'],
   }
   
-  file { ["/var/www", "/var/www/puppetagent.dev"]:
-    ensure => "directory",
+  file { ['/var/www', '/var/www/puppetagent.dev']:
+    ensure => 'directory',
   }
   
   file { '/var/www/puppetagent.dev/index.html':
